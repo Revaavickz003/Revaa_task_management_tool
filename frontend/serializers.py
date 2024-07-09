a58998590b419b33cfdc521e0b24ce5b1e103f35
nitish-1622 <niti16082002@gmail.com>
2024-07-09 11:14:13 +0530

    Delete frontend directory

--- frontend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TaskSheet
-
-class TaskSheetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TaskSheet
-        fields = '__all__'
